a4413280111b6df4020f4f2015260f458f90ea42
Diogo Oliveira <dluks82@gmail.com>
2024-09-03 12:06:19 -0300

    refact: Create a `Screen` class to show menus

---
+++ src/ui/Screen.java
@@ -0,0 +1,55 @@
+package ui;
+
+public class Screen {
+    public static void exibirMenu(int tamanhoAtual) {
+        String contador = String.format("%04d", tamanhoAtual);
+
+        System.out.println("""
+                ╔══════════════════════════════════════════════════╗
+                ║             _                    _               ║
+                ║            /_\\  __ _ ___ _ _  __| |__ _          ║
+                ║           / _ \\/ _` / -_) ' \\/ _` / _` |         ║
+                ║          /_/ \\_\\__, \\___|_||_\\__,_\\__,_|         ║
+                ║                |___/                             ║
+                ║          Coders 2024       versão: 1.0.0         ║
+                ╠══════════════════════════════════════════════════╣
+                ║                  >>>> MENU <<<<                  ║
+                ║                                                  ║
+                ║        [1] - Adicionar Contato                   ║
+                ║        [2] - Remover Contato                     ║
+                ║        [3] - Detalhar Contato                    ║
+                ║        [4] - Editar Contato                      ║
+                ║        [5] - Listar Contatos                     ║
+                ║        [6] - Sobre                               ║
+                ║                                                  ║
+                ║        [9] - Sair                                ║
+                ╚══════════════════════════════ Contatos""" + " " + contador + " ═════╝");
+    }
+
+    public static void exibirSobre() {
+        System.out.println("""
+                ╔══════════════════════════════════════════════════╗
+                ║             _                    _               ║
+                ║            /_\\  __ _ ___ _ _  __| |__ _          ║
+                ║           / _ \\/ _` / -_) ' \\/ _` / _` |         ║
+                ║          /_/ \\_\\__, \\___|_||_\\__,_\\__,_|         ║
+                ║                |___/                             ║
+                ║          Coders 2024       versão: 1.0.0         ║
+                ╠══════════════════════════════════════════════════╣
+                ║                  >>>> SOBRE <<<<                 ║
+                ║                                                  ║
+                ║    A aplicação Agenda foi desenvolvida como      ║
+                ║  projeto de conclusão do módulo 'Lógica de       ║
+                ║  Programação I', no curso 'Santander Coders      ║
+                ║  2024.1 - Back-End'.                             ║
+                ║                                                  ║
+                ║                 COLABORADORES                    ║
+                ║      -  Diogo Lucas de Oliveira                  ║
+                ║      -  Eloise Helena Moraes                     ║
+                ║      -  Irving Lui                               ║
+                ║      -  Isaque Menezes                           ║
+                ║                                                  ║
+                ╚══════════════════════════════════════════════════╝""");
+    }
+
+}
