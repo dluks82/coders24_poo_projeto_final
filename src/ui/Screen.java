d8797f1e9b8a5bccb11d08e609d0ac48495ddb82
Diogo Oliveira <dluks82@gmail.com>
2024-09-05 14:58:34 -0300

    refact: Remove useless code colors

--- src/ui/Screen.java
+++ src/ui/Screen.java
@@ -2,17 +2,6 @@ package ui;
 
 public class Screen {
 
-    // Códigos ANSI para cores
-    public static final String RESET = "\u001B[0m";
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
-
     static final int ID_COLUMN_WIDTH = 2;
     static final int NAME_COLUMN_WIDTH = 30;
     static final int PHONE_COLUMN_WIDTH = 20;
