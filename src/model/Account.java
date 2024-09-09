ce7021238969884b554f46127ebcd39e0079baa5
Diogo Oliveira <dluks82@gmail.com>
2024-09-09 13:10:39 -0300

    refact: remove unused setters and use new exception

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,7 @@
 package model;
 
+import exception.InsufficientBalanceException;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.security.MessageDigest;
@@ -8,9 +10,9 @@ import java.security.NoSuchAlgorithmException;
 public abstract class Account implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String number;
+    private final String number;
     private BigDecimal balance;
-    private String ownerId;
+    private final String ownerId;
     private String password;
 
     public Account(String number, BigDecimal balance, String ownerId, String password) {
@@ -24,24 +26,12 @@ public abstract class Account implements Serializable {
         return number;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
     public BigDecimal getBalance() {
         return balance;
     }
 
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
     public String getOwnerId() {
         return ownerId;
-    }
-
-    public void setOwnerId(String ownerId) {
-        this.ownerId = ownerId;
     }
 
     public void setPassword(String password) {
@@ -49,6 +39,8 @@ public abstract class Account implements Serializable {
     }
 
     public boolean deposit(BigDecimal amount) {
+        if (amount == null) throw new IllegalArgumentException("Valor não pode ser nulo");
+
         if (amount.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("Valor não pode ser negativo!");
         }
@@ -58,18 +50,18 @@ public abstract class Account implements Serializable {
     }
 
     public boolean withdrawal(BigDecimal amount) {
+        if (amount == null) throw new IllegalArgumentException("Valor não pode ser nulo");
+
         if (amount.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("Valor não pode ser negativo!");
         }
-        if (amount.compareTo(this.balance) <= 0) {
+        if (this.balance.compareTo(amount) >= 0) {
             this.balance = this.balance.subtract(amount);
             return true;
         } else {
-            // lançar uma exceção
-            return false;
+            throw new InsufficientBalanceException();
         }
     }
-
 
     @Override
     public String toString() {
